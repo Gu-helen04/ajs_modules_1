efb0eb584e63359b44b6d598d6371b9775fb55cc
Helen <gu-helen04@mail.ru>
2024-02-04 23:13:26 +0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmpWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugun = require('mini-css-extract-plugin');
+
+module.exports ={
+    entry: [
+        './src/index.js',
+        './src/js/app.js',
+    ],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.js'
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use: [
+                    MiniCSSExtractPlugun.loader,
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins:[
+        new HtmpWebpackPlugin({
+            template: './src/index.html',
+            filename: 'main.html'
+        }),
+        new MiniCSSExtractPlugun()
+    ],
+    devServer: {
+        static: {
+          directory: path.join(__dirname,'dist'),
+        }
+      }
+}
